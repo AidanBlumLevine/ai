94b22512f4eb98e97c94623240a96bdf67198b5a
AidanBlumLevine <31904615+AidanBlumLevine@users.noreply.github.com>
2017-11-03 19:22:53 -0400

    vnhjghj

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,4 +1,39 @@
+import java.util.ArrayList;
 
 public class NeuralNetwork {
-
+	private ArrayList<HiddenNode> hiddenLayer = new ArrayList<>();
+	private ArrayList<InputNode> inputLayer = new ArrayList<>();
+	private ArrayList<OutputNode> outputLayer = new ArrayList<>();
+	public NeuralNetwork(int inputs,int hidden,int outputs) {
+		ArrayList<Weight> weights = new ArrayList<>();
+		
+		for(int i=0;i<inputs;i++) {
+			inputLayer.add(new InputNode());
+		}
+		InputNode b = new InputNode();
+		b.setValue(1);
+		
+		for(int i=0;i<(inputs*hidden)+(hidden*outputs)+hidden+outputs;i++){
+			weights.add(new Weight(1));
+		}
+		
+		for(int i=0;i<hidden;i++) {
+			Weight[] inputWeights = new Weight[inputs];
+			Weight[] outputWeights = new Weight[outputs];
+			for(int n=0;n<inputs;n++) {
+				inputWeights[n] = weights.get(n);
+			}
+			for(int n=0;n<inputs;n++) {
+				weights.remove(0);
+			}
+			for(int n=0;n<outputs;n++) {
+				outputWeights[n] = weights.get(n);
+			}
+			for(int n=0;n<outputs;n++) {
+				weights.remove(0);
+			}
+			HiddenNode h = new HiddenNode(inputWeights,outputWeights);
+			hiddenLayer.add(h);
+		}
+	}	
 }
